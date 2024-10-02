c0b03991f98de80e91fddfb977bb974a0774af42
Hunter W. Lane <scoompers@cs.nmt.edu>
2024-10-02 11:05:10 -0600

    Hello world program using system calls.

--- p1/program.s
+++ p1/program.s
@@ -1,8 +1,17 @@
 .section .note.GNU-stack,"",@progbits
 
 .section .data
-
+	msg: .string "Hello world\n"
 .section .text
+
+.global _start
+_start:
+	movq $1, %rax
+	movq $1, %rdi
+	lea msg, %rsi
+	movq $12, %rdx
+	syscall
+exit:
 	movq $60, %rax
 	movq $0, %rdi
 	syscall
